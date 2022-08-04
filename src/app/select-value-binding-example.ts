80b6b6c0b5862c26ec61bf37879eeb7e6214315e
SaitarunPatel <tarunp537@gmail.com>
2022-08-05 02:05:10 +0530

    3 files modified

--- src/app/select-value-binding-example.ts
+++ src/app/select-value-binding-example.ts
@@ -1,15 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 /** @title Select with 2-way value binding */
 @Component({
   selector: 'select-value-binding-example',
   templateUrl: 'select-value-binding-example.html',
-  styleUrls: ['./select-value-binding-example.css']
+  styleUrls: ['./select-value-binding-example.css'],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class SelectValueBindingExample {
   selected = 'option2';
 }
-
+// ViewEncapsulation.None
 
 /**  Copyright 2022 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
